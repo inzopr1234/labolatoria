6b22f3dd311cb2dbff25e8b03c76054f82fb4807
unknown <inf132214@lab-44-4.cs.put.poznan.pl>
2018-10-30 15:49:22 +0100

    asda
    a1

--- Program.java
+++ Program.java
@@ -1,9 +1,10 @@
-
 public class HelloWorld {
 
     public static void main(String[] args) {
+		//kom2
         // Prints "Hello, World" to the terminal window.
         System.out.println("HELLO, World!");
+		//kom2
     }
 
 }
